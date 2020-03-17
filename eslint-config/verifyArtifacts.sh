77efb606a6e5e36c167538a9c7fbce3a97b59be6
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-17 10:13:50 -0400

    Add circle ci config and basic tests

---
+++ eslint-config/verifyArtifacts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+GRAY='\033[1;30m'
+NC='\033[0m' # No Color
+
+
+echo "Checking ESlint Config Required Files"
+echo -ne "  readme: "
+if [ ! -f ./README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  package.json: "
+if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  license: "
+if [ ! -f ./license.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  index: "
+if [ ! -f ./index.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  ts: "
+if [ ! -f ./ts.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  tsx: "
+if [ ! -f ./tsx.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  pxb-rules: "
+if [ ! -f ./pxb-rules.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
