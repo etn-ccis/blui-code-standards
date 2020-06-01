cfa8df3013ff3e9e0169ea49944d547b153bb297
joebochill <boyle.p.joseph@gmail.com>
2020-06-01 14:36:43 -0400

    Update ts.js

--- eslint-config/ts.js
+++ eslint-config/ts.js
@@ -2,7 +2,6 @@ const tsProfile = {
     parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
     extends:  [
         'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'eslint-config-prettier'
